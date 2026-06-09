95f6d4ab02e849a2681974a9dfb73f18005ab378
Sivaji_Singiri <147472854+SivajiSingiri1619@users.noreply.github.com>
2026-06-09 21:05:46 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+         int ans = 0;
+        int n = prices.size();
+        int maxi = prices[n-1];
+        for(int i=n-2;i>=0;i--){
+
+             if(maxi > prices[i]) ans = max(ans,maxi - prices[i]);
+             else maxi = prices[i];
+        }
+        return ans;
+    }
+};
